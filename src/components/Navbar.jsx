8e3ce93055aa462185dc69d7af68cd1fff876b13
pixelshot <pixelshot8@gmail.com>
2022-08-09 11:12:34 +0800

    Changed text to blue

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 const Navbar = () => {
   return (
-    <div className="fuxed w-full h-[80px] flex justif-between items-center px-4 bg-[#0a192f] text-gray-300">
+    <div className="fuxed w-full h-[80px] flex justif-between items-center px-4 bg-[#0a192f] text-blue-300">
       Navbar
     </div>
   );
